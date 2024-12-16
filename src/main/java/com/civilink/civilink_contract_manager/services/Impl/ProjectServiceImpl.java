bcea152ee639822639fc9a6919a2bfee2696f5e2
yasirulakshitha <164196493+yasirulakshitha@users.noreply.github.com>
2024-12-16 19:47:07 +0530

    -project find by id in service class created -update project method created in service class -project find by id get endpoint created -update project endpoint created

--- src/main/java/com/civilink/civilink_contract_manager/services/Impl/ProjectServiceImpl.java
+++ src/main/java/com/civilink/civilink_contract_manager/services/Impl/ProjectServiceImpl.java
@@ -1,6 +1,8 @@
 package com.civilink.civilink_contract_manager.services.Impl;
 
 import com.civilink.civilink_contract_manager.dtos.requests.RequestProjectDto;
+import com.civilink.civilink_contract_manager.dtos.requests.RequestProjectFindByIdDto;
+import com.civilink.civilink_contract_manager.dtos.requests.RequestProjectUpdateDto;
 import com.civilink.civilink_contract_manager.entities.Client;
 import com.civilink.civilink_contract_manager.entities.Project;
 import com.civilink.civilink_contract_manager.repositories.ClientRepository;
@@ -8,6 +10,8 @@ import com.civilink.civilink_contract_manager.repositories.ProjectRepository;
 import com.civilink.civilink_contract_manager.services.ProjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -38,5 +42,33 @@ public class ProjectServiceImpl implements ProjectService {
 
         projectRepository.save(project);
 
+    }
+
+    @Override
+    public void updateProject(RequestProjectUpdateDto requestProjectUpdateDto) {
+        RequestProjectFindByIdDto requestProjectFindByIdDto = new RequestProjectFindByIdDto(requestProjectUpdateDto.getId());
+        Project project = projectRepository.findById(requestProjectFindByIdDto.getId()).get();
+
+        project.setProjectCategory(requestProjectUpdateDto.getProjectCategory());
+        project.setProjectEndDate(requestProjectUpdateDto.getProjectEndDate());
+        project.setProjectName(requestProjectUpdateDto.getProjectName());
+        project.setProjectStartDate(requestProjectUpdateDto.getProjectStartDate());
+        project.setProjectDescription(requestProjectUpdateDto.getProjectDescription());
+        project.setProjectNumber(requestProjectUpdateDto.getProjectNumber());
+        project.setProjectStatus(requestProjectUpdateDto.getProjectStatus());
+        project.setId(requestProjectFindByIdDto.getId());
+        projectRepository.save(project);
+
+
+    }
+
+    @Override
+    public Project findById(RequestProjectFindByIdDto requestProjectFindByIdDto) {
+        Optional<Project> optional =  projectRepository.findById(requestProjectFindByIdDto.getId());
+
+        if (optional.isPresent()) {
+            return optional.get();
+        }
+        return null;
     }
 }
