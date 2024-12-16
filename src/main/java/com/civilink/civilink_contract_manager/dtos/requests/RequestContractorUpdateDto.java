254805f9afc75541386d303283ebeb6a90959ae4
Akash Welgama <139337308+Akashwelgama@users.noreply.github.com>
2024-12-16 21:48:53 +0530

    version 3
    Minor updates on Contractor CRUD operations

---
+++ src/main/java/com/civilink/civilink_contract_manager/dtos/requests/RequestContractorUpdateDto.java
@@ -0,0 +1,13 @@
+package com.civilink.civilink_contract_manager.dtos.requests;
+
+import java.util.List;
+
+public class RequestContractorUpdateDto {
+    private String id;
+    private String name;
+    private String contact;
+    private List<String> bidResponses;
+
+    private List<String> posts;
+
+}
