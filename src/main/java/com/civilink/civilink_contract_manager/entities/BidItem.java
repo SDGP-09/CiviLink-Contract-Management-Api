daf6f4c4f10085cd082c3febeafe264773221a79
yasirulakshitha <gamagejanithyasirulakshitha@gmail.com>
2025-03-11 14:08:27 -0700

    -remove all data anotation and replace with getters and setters for all request and response Dto's

--- src/main/java/com/civilink/civilink_contract_manager/entities/BidItem.java
+++ src/main/java/com/civilink/civilink_contract_manager/entities/BidItem.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 
 
 @Entity
-@Data
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
@@ -20,7 +20,39 @@ public class BidItem {
     private String description;
     private String url;
 
-//    @ManyToOne
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    //    @ManyToOne
 //    @JoinColumn(name = "bid_invitation_id")
 //    private BidInvitation bidInvitation;
 
--- src/main/java/com/civilink/civilink_contract_manager/entities/Project.java
+++ src/main/java/com/civilink/civilink_contract_manager/entities/Project.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+
 @Builder
 public class Project {
     @Id
@@ -25,6 +25,70 @@ public class Project {
     private String projectEndDate;
     private String projectStatus;
     private String projectCategory;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getProjectNumber() {
+        return projectNumber;
+    }
+
+    public void setProjectNumber(int projectNumber) {
+        this.projectNumber = projectNumber;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getProjectDescription() {
+        return projectDescription;
+    }
+
+    public void setProjectDescription(String projectDescription) {
+        this.projectDescription = projectDescription;
+    }
+
+    public String getProjectStartDate() {
+        return projectStartDate;
+    }
+
+    public void setProjectStartDate(String projectStartDate) {
+        this.projectStartDate = projectStartDate;
+    }
+
+    public String getProjectEndDate() {
+        return projectEndDate;
+    }
+
+    public void setProjectEndDate(String projectEndDate) {
+        this.projectEndDate = projectEndDate;
+    }
+
+    public String getProjectStatus() {
+        return projectStatus;
+    }
+
+    public void setProjectStatus(String projectStatus) {
+        this.projectStatus = projectStatus;
+    }
+
+    public String getProjectCategory() {
+        return projectCategory;
+    }
+
+    public void setProjectCategory(String projectCategory) {
+        this.projectCategory = projectCategory;
+    }
 
     // Relationship to Bid
 //    @ManyToOne
